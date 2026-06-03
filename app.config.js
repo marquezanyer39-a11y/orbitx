0434ebb0feb87e87ed4f916c6d00d9cf2ea29045
Anyer Marquez <marquezanyer39@gmail.com>
2026-06-02 20:16:20 -0500

    chore(config): update expo app config for qvex

--- app.config.js
+++ app.config.js
@@ -1,12 +1,20 @@
 const appJson = require('./app.json');
 
-module.exports = () => ({
-  ...appJson.expo,
-  plugins: [...(appJson.expo.plugins ?? []), './plugins/withWalletQueries'],
-  extra: {
-    ...appJson.expo.extra,
-    orbitxBackendUrl: process.env.EXPO_PUBLIC_ORBITX_BACKEND_URL ?? '',
-    astraBackendUrl: process.env.EXPO_PUBLIC_ASTRA_VOICE_API_URL ?? '',
-    walletConnectProjectId: process.env.EXPO_PUBLIC_WALLETCONNECT_PROJECT_ID ?? '',
-  },
-});
+module.exports = ({ config } = {}) => {
+  const baseConfig = {
+    ...appJson.expo,
+    ...(config ?? {}),
+  };
+  const plugins = [...(baseConfig.plugins ?? []), 'expo-font', './plugins/withWalletQueries'];
+
+  return {
+    ...baseConfig,
+    plugins: Array.from(new Set(plugins)),
+    extra: {
+      ...(baseConfig.extra ?? {}),
+      orbitxBackendUrl: process.env.EXPO_PUBLIC_ORBITX_BACKEND_URL ?? '',
+      astraBackendUrl: process.env.EXPO_PUBLIC_ASTRA_VOICE_API_URL ?? '',
+      walletConnectProjectId: process.env.EXPO_PUBLIC_WALLETCONNECT_PROJECT_ID ?? '',
+    },
+  };
+};
