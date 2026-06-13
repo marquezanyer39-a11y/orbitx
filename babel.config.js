89c98f605f9aa8aca6dfba55b992931a05c0575d
Anyer Marquez <marquezanyer39@gmail.com>
2026-06-12 21:14:13 -0500

    chore(client): update babel reanimated plugin configuration

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = function (api) {
     presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
     plugins: [
       '@babel/plugin-proposal-export-namespace-from',
-      'react-native-worklets/plugin',
+      'react-native-reanimated/plugin',
     ],
   };
 };
